ff0bc5d611e5be07e46332e68c499337a5788fd0
Cal <43823915+CalRL@users.noreply.github.com>
2021-11-22 20:32:19 +0100

    Update configurator.php

--- configurator.php
+++ configurator.php
@@ -9,17 +9,7 @@
   <input type = "color" value = "#000000" id = "clr" onchange = "docolor()">
     <input type = "range" min = "10" max = "100" value = "10" id = "soft" oninput = "dosquare()">
     </p>
-    <style type="text/css">
-    #color {
-    width: 200pt;
-    height: 50pt;
-    border: 2px solid #fff;
-  }
-  .btn{padding: 10px; color: #fff;background:#000;cursor: pointer;}
-  .yellowback {
-    background-color: yellow;
-  }
-    </style>
+<link rel="StyleSheet" href="configurator.css" type="text/css"/>
 <script type="text/javascript" src="configurator.js"></script>
 </div>
 </html>
